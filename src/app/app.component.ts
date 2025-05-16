e5e27d61e43945a93446c79063926714cd5457ce
ManoelOscar35 <manoeloscar557@gmail.com>
2025-05-16 12:15:39 -0300

    feat(layout): integrates header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { LayoutModule } from '@ecommerce/layout';
+
 
 @Component({
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [
+    RouterModule,
+    LayoutModule
+  ],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
